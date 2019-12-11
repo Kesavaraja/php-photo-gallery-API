677bd7743114fc2926ea05237619ed4cf35d7634
Evan Genest <genest.1@osu.edu>
2019-12-11 14:08:02 -0600

    Add formatted print of array keys.

--- syntax/classExample.php
+++ syntax/classExample.php
@@ -1,25 +1,19 @@
 <?php 
 echo "Hubba hubba hubba.";
 
-class Notebooks {
 
-	function getTool() {
-		echo "pen";
-	}
 
-	function getVolume() {
-		echo "yellow";
-	}
-}
-
-$the_methods = get_class_methods('Notebooks')
-
+$the_methods = get_class_methods('Notebooks');
 $book1 = array( 'cover' => 'blue' ,'topic' => 'css' ,'year' => '2017' ); 
 $book2 = array( 'cover' => 'yellow' ,'topic' => 'react' ,'year' => '2019' );
 $book3 = array( 'cover' => 'red' ,'topic' => 'elm' ,'year' => '2018' );
 $book3 = array( 'cover' => 'green' ,'topic' => 'nginx' ,'year' => '2018' );
 $book4 = array( 'cover' => 'red' ,'topic' => 'elm' ,'year' => '2018' ); 
-$the_books = array(book1, book2, book3, book4);
+$the_books = array($book1, $book2, $book3, $book4);
 
-echo $the_books[0];
+
+foreach ($the_books as $key => $value) {
+	printf("%s yeah %s <br/>", $key, $value['topic']);
+}
+
 ?>
