a7e25c06adb3e8414f0c462d4e8a16aa47ee2518
Evan Genest <genest.1@osu.edu>
2019-12-11 16:18:43 -0600

    Put getter and setter on Book class.

--- syntax/classExample.php
+++ syntax/classExample.php
@@ -1,19 +1,38 @@
 <?php 
 echo "Hubba hubba hubba.";
 
+class Book {
+	// properties
+	public $title; 
+	public $pages;
+	public $year;
+
+	function set_title($title){
+		$this->title = $title;
+	}
+
+	function get_title(){
+		return $this->title;
+	}
+
+}
 
 
-$the_methods = get_class_methods('Notebooks');
-$book1 = array( 'cover' => 'blue' ,'topic' => 'css' ,'year' => '2017' ); 
-$book2 = array( 'cover' => 'yellow' ,'topic' => 'react' ,'year' => '2019' );
-$book3 = array( 'cover' => 'red' ,'topic' => 'elm' ,'year' => '2018' );
-$book3 = array( 'cover' => 'green' ,'topic' => 'nginx' ,'year' => '2018' );
-$book4 = array( 'cover' => 'red' ,'topic' => 'elm' ,'year' => '2018' ); 
+// class Booklist;
+
+
+$the_methods = get_class_methods('Book');
+$book1 = array( 'pages' => 325,'title' => 'css' ,'year' => '2017' ); 
+$book2 = array( 'pages' => 325,'title' => 'react' ,'year' => '2019' );
+$book3 = array( 'pages' => 325,'title' => 'elm' ,'year' => '2018' );
+$book3 = array( 'pages' => 325,'title' => 'nginx' ,'year' => '2018' );
+$book4 = array( 'pages' => 325,'title' => 'elm' ,'year' => '2018' ); 
 $the_books = array($book1, $book2, $book3, $book4);
 
+print("\n");
 
 foreach ($the_books as $key => $value) {
-	printf("%s yeah %s <br/>", $key, $value['topic']);
+	printf("Book %s is about %s <br/>", $key, $value['title']);
 }
 
 ?>
