906ad2acb0a77fabf19e494208e5fc87afa18d47
Evan Genest <genest.1@osu.edu>
2019-12-13 20:05:13 -0600

    Refactor SQL helpers to use generic SQL finder.

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,23 +2,22 @@
 
 class User {
 
-  public function find_all_users(){
-    global $database;
-    $result_set = $database->query("SELECT * FROM users");
-    return $result_set;
+  public static function find_all_users(){
+    return self::find_any("SELECT * FROM users");
   }
 
 // User::find_user_byID
 
-  public function find_user_byID($n){
-    global $database;
-    $result_mono = $database->query(
-      "SELECT * FROM users WHERE id= $n");
-    // return $result_mono; Ohhhh! Need a conversion here.
-    $the_found_user = mysqli_fetch_array($result_mono);
-    return $the_found_user; 
+  public static function find_user_byID($n){
+    $results_as_mysqli = self::find_any("SELECT * FROM users WHERE id= $n");
+    return mysqli_fetch_array($results_as_mysqli);
   }
 
+public static function find_any($sql){
+  global $database;
+  $result_set = $database->query($sql);
+  return $result_set;
+} 
 
 }
 
