d60adcad1daddf2b0cf64882ce139a078620fc62
Evan Genest <genest.1@osu.edu>
2020-01-19 12:01:58 -0600

    Add URL module. Output keys of req object to screen.

--- get-exp/get.js
+++ get-exp/get.js
@@ -1,18 +1,12 @@
 const http = require('http');
+const url = require('url');
 
 const block = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1 style="background-color: gray; font-family: helvetica; color: yellow"><form action="/action_page.php" method="get">First name: <input type="text" name="fname"><br>Last name: <input type="text" name="lname"><br><input type="submit" value="Submit"></form></h1>');
-
-
-
-
-   //write a response to the client
+  res.write('<h1 style="background-color: gray; font-family: helvetica; color: yellow"><form  method="get">First name: <input type="text" name="fname"><br>Last name: <input type="text" name="lname"><br><input type="submit" value="Submit"></form></h1>');
+  var q = url.parse(req.url, true);
+  res.write("Size of q object is " + Object.keys(q));
   res.end(); 
 }
 
 http.createServer(block).listen(4321, ()=>{console.log("listening on 4321")});
-
-
-
-
