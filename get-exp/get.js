c6c10fa1ad9948e718ce27f4e7c32ce310219e39
Evan Genest <genest.1@osu.edu>
2020-01-19 11:44:32 -0600

    Write text form with submit button.

--- get-exp/get.js
+++ get-exp/get.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 const block = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1 style="background-color: gray; font-family: helvetica; color: yellow">Hello </h1>'); //write a response to the client
+  res.write('<h1 style="background-color: gray; font-family: helvetica; color: yellow"><form action="/action_page.php" method="get">First name: <input type="text" name="fname"><br>Last name: <input type="text" name="lname"><br><input type="submit" value="Submit"></form></h1>');
+
+
+
+
+   //write a response to the client
   res.end(); 
 }
 
 http.createServer(block).listen(4321, ()=>{console.log("listening on 4321")});
+
+
+
+
