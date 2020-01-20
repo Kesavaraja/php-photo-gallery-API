ff575983f95c91a04c1fbb4da72f2a04fc672296
Evan Genest <genest.1@osu.edu>
2020-01-19 19:47:07 -0600

    Use JSON.stringify to loop through and print the parsed URL object.

--- get-exp/get.js
+++ get-exp/get.js
@@ -4,8 +4,15 @@ const url = require('url');
 const block = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1 style="background-color: gray; font-family: helvetica; color: yellow"><form  method="get">First name: <input type="text" name="fname"><br>Last name: <input type="text" name="lname"><br><input type="submit" value="Submit"></form></h1>');
-  var q = url.parse(req.url, true);
-  res.write("Size of q object is " + Object.keys(q));
+  let q = url.parse(req.url, true);
+  // res.write("Size of q object is " + Object.keys(q));
+  let elements = "<p>";
+  let keys = Object.keys(q);
+  for(let i = 0; i < keys.length; i++){
+    elements += "Key: " + keys[i] + "key is an: " + typeof keys[i] + "Q[keys[i]] is : " +  JSON.stringify( q[keys[i]]) +  "<br />";
+  }
+  elements += "</p>"
+  res.write(elements);
   res.end(); 
 }
 
